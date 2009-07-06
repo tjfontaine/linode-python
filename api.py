f9b10ea70f7a370cfc922d95a46fb29a0991fce0
Timothy J Fontaine <tjfontaine@gmail.com>
2009-07-06 18:51:34 -0400

    the beginnings of apiv2

--- api.py
+++ api.py
@@ -52,7 +52,7 @@ class ApiInfo:
   valid_commands = {}
   valid_params   = {}
 
-LINODE_API_URL = 'https://api.linode.com/api/'
+LINODE_API_URL = 'https://beta.linode.com/api/'
 
 class Api:
   def __init__(self, key, debug=False, batching=False):
@@ -98,14 +98,19 @@ class Api:
 
   def __send_request(self, request):
     request['api_key'] = self.__key
-    request['resultFormat'] = 'json'
+    request['api_resultFormat'] = 'json'
     request = urllib.urlencode(request)
     self.debug('Sending '+request)
     req = self.__request(LINODE_API_URL, request)
     response = self.__urlopen(req)
     response = response.read()
     self.debug('Received '+response)
-    json = simplejson.loads(response)
+    try:
+      json = simplejson.loads(response)
+    except Exception, ex:
+      print response
+      raise ex
+
     if type(json) is dict:
       if len(json['ERRORARRAY']) > 0:
         raise ApiError(json['ERRORARRAY'])
@@ -130,7 +135,7 @@ class Api:
         ApiInfo.valid_commands[func.__name__] = True
 
       def wrapper(self, mparams = {}, *__args ,**__kw):
-        request = {'action' : func.__name__}
+        request = {'api_action' : func.__name__.replace('_', '.')}
 
         if len(mparams) == 0: #only self, no passed parameters
           params = __kw
@@ -165,203 +170,134 @@ class Api:
       return wrapper
     return decorator
 
-  @__api_request()
-  def domainList(self, request):
-    """Retrieve the list of domains visible to the user.
+  @__api_request(optional=['LinodeID'])
+  def linode_list(self, request):
+    pass
 
-    Parameters:
-      None
+  @__api_request(required=['LinodeID'], optional=['Label',
+                                                  'lpm_displayGroup',
+                                                  'Alert_cpu_enabled',
+                                                  'Alert_cpu_threshold',
+                                                  'Alert_diskio_enabled',
+                                                  'Alert_diskio_threshold',
+                                                  'Alert_bwin_enabled',
+                                                  'Alert_bwin_threshold',
+                                                  'Alert_bwout_enabled',
+                                                  'Alert_bwout_threshold',
+                                                  'Alert_bwquota_enabled',
+                                                  'Alert_bwquota_threshold',
+                                                  'backupWindow',
+                                                  'backupWeeklyDay',
+                                                  'watchdog',
+                                                 ])
+  def linode_update(self, request):
+    pass
 
-    Returns an array of hashes, one per domain, each with the following fields:
-      DOMAINID       - Unique identifier for the domain
-      DOMAIN         - Domain's name e.g. 'linode.com'
-      TYPE           - Domain's type: 'master' or 'slave'
-      STATUS         - Domain's current status
-        * Possible values are listed below
-      SOA_EMAIL      - SOA email address for the domain
-      REFRESH_SEC *  - 'refresh' value for the domain
-        * A value of zero indicates default time (2 hours)
-      RETRY_SEC *    - 'retry' value for the domain
-        * A value of zero indicates default time (2 hours)
-      TTL_SEC *      - 'ttl' value for the domain
-        * A value of zero indicates default time (1 day)
+  @__api_request(required=['DatacenterID', 'PlanID', 'PaymentTerm'])
+  def linode_create(self, request):
+    pass
 
-    Possible values for STATUS
-      0 - Disabled   - Domain is not being served
-      1 - Active     - Domain is being served
-      2 - Edit Mode  - Domain is being served but changes are not rendered
-      3 - Has Errors - There are errors in the rendered zonefile
-    
-    """
+  @__api_request(required=['LinodeID'])
+  def linode_shutdown(self, request):
+    pass
 
-  @__api_request(['DomainID'])
-  def domainGet(self, request):
-    """Retrieve the details for a specific domain.
+  @__api_request(required=['LinodeID'], optional=['LinodeConfigID'])
+  def linode_boot(self, request):
+    pass
 
-    Parameters:
-      DomainID       - Unique identifier for the domain to be retrieved
-        * Always required
+  @__api_request(required=['LinodeID'])
+  def linode_delete(self, request):
+    pass
 
-    Returns a hash for the requested domain
-      * See domainList documentation for the fields in the hash
-    
-    """
+  @__api_request(required=['LinodeID'], optional=['LinodeConfigID'])
+  def linode_reboot(self, request):
+    pass
 
-  @__api_request(['DomainID', 'Domain', 'Type', 'Status', 'SOA_Email'], ['Master_IPs', 'Refresh_Sec', 'Retry_Sec', 'TTL_Sec'])
-  def domainSave(self, request):
-    """Create or update a specific domain.
+  @__api_request(required=['LinodeID'])
+  def linode_config_list(self, request):
+    pass
 
-    Parameters:
-      DomainID       - Unique identifier for the domain
-        * Always required - use 0 to insert a new domain
-      Domain         - Domain's name e.g. 'linode.com'
-        * Always required
-      Type           - Domain's type: 'master' or 'slave'
-        * Always required
-      Status         - Domain's new status
-        * Always required - see domainList documentation for possible values
-      SOA_Email      - SOA email address for the domain
-        * Always required
-      Master_IPs     - Semicolon separated list of IP(s) for master servers
-        * Required if Type = 'slave', ignored otherwise
-      Refresh_Sec    - 'refresh' value for the domain
-        * Excluding or setting to zero indicates default time (2 hours)
-      Retry_Sec      - 'retry' value for the domain
-        * Excluding or setting to zero indicates default time (2 hours)
-      TTL_Sec        - 'ttl' value for the domain
-        * Excluding or setting to zero indicates default time (1 day)
+  @__api_request(required=['LinodeID', 'LinodeConfigID'], optional=[
+                                                            'KernelID',
+                                                            'Label',
+                                                            'Comments',
+                                                            'RAMLimit',
+                                                            'DiskList',
+                                                            'RunLevel',
+                                                            'RootDeviceNum',
+                                                            'RootDeviceCustom',
+                                                            'RootDeviceRO',
+                                                            'helper_disableUpdateDB',
+                                                            'helper_xen',
+                                                            'helper_depmod',
+                                                          ])
+  def linode_config_update(self, request):
+    pass
 
-    Returned fields:
-      DOMAINID       - Unique identifier for the new or updated domain
+  @__api_request(required=['LinodeID', 'KernelID'], optional=[
+                                                            'Label',
+                                                            'Comments',
+                                                            'RAMLimit',
+                                                            'DiskList',
+                                                            'RunLevel',
+                                                            'RootDeviceNum',
+                                                            'RootDeviceCustom',
+                                                            'RootDeviceRO',
+                                                            'helper_disableUpdateDB',
+                                                            'helper_xen',
+                                                            'helper_depmod',
+                                                          ])
+  def linode_config_create(self, request):
+    pass
 
-    *** Parameters not passed to update a domain will be reset to defaults ***
+  @__api_request(required=['LinodeID', 'LinodeConfigID'])
+  def linode_config_delete(self, request):
+    pass
+  
+  @__api_request(required=['LinodeID'])
+  def linode_disk_list(self, request):
+    pass
 
-    """
-    self.debug(request)
-    if request['type'].lower() == 'master':
-      if not request.has_key('soa_email'):
-        raise MissingRequiredArgument('soa_email')
-    elif request['type'].lower() == 'slave':
-      if not request.has_key('master_ips'):
-        raise MissingRequiredArgument('master_ips')
-    else:
-      raise Exception('Zone Type is not master or slave')
+  @__api_request(required=['LinodeID', 'DiskID'], optional=['Label', 'isReadOnly'])
+  def linode_disk_update(self, request):
+    pass
 
-    return request
+  @__api_request(required=['LinodeID', 'Type', 'Size'], optional=['Label', 'isReadOnly'])
+  def linode_disk_create(self, request):
+    pass
 
-  @__api_request(['DomainID'])
-  def domainResourceList(self, request):
-    """Retrieve the list of resource records (RRs) for a specific domain.
-    
-    Parameters:
-      DomainID       - Unique identifier for the domain
-        * Always required
+  @__api_request(required=['LinodeID', 'DiskID'])
+  def linode_disk_duplicate(self, request):
+    pass
 
-    Returns an array of hashes, one per RR, each with the following fields:
-      RESOURCEID     - Unique identifier for the RR
-      DOMAINID       - Unique identifier for the domain
-      NAME           - Name of the RR
-        * May be empty
-      TYPE           - Type of the RR
-        * Possible values are listed below
-      TARGET         - IP, name or string this RR resolves to
-      PRIORITY       - Priority for MX type RRs
-      TTL_SEC        - 'ttl' value for the RR
-        * A value of zero indicates the domain default
-      WEIGHT         - Weight for SRV type RRs
-      PORT           - Port for SRV type RRs
-    
-    Possible values for RR TYPE
-      NS             - Name server
-      MX             - Mail exchanger
-      A              - IPv4 address
-      AAAA           - IPv6 address
-      CNAME          - Canonical name
-      TXT            - Text
-      SRV            - Service location
-    
-    """
+  @__api_request(required=['LinodeID', 'DiskID'])
+  def linode_disk_delete(self, request):
+    pass
 
-  @__api_request(['ResourceID'])
-  def domainResourceGet(self, request):
-    """Retrieve the details for a specific resource record (RR).
+  @__api_request(required=['LinodeID', 'DiskID', 'Size'])
+  def linode_disk_resize(self, request):
+    pass
 
-    Parameters:
-      ResourceID     - Unique identifier for the domain to be retrieved
-        * Always required
+  @__api_request(required=['LinodeID', 'DistributionID', 'rootPass', 'Label', 'Size'])
+  def linode_disk_createfromdistribution(self, request):
+    pass
 
-    Returns a hash for the requested RR
-      * See domainResourceList documentation for the fields in the hash
-      
-    """
+  @__api_request(required=['LinodeID'], optional=['IPAddressID'])
+  def linode_ip_list(self, request):
+    pass
 
-  @__api_request(['ResourceID', 'DomainID', 'Type', 'Target', 'Name'], ['Priority', 'TTL_Sec', 'Weight', 'Port'])
-  def domainResourceSave(self, request):
-    """Create or update a specific resource record (RR).
-    
-    Parameters:
-      ResourceID     - Unique identifier for the RR
-        * Always required - use 0 to insert a new RR
-      DomainID       - Unique identifier for the domain
-        * Always required.
-      Name           - Name of the RR
-        * May be empty
-      Type           - Type of the RR
-        * Always required - see domainResourceList documentation for possible values
-      Target         - IP, name or string this RR resolves to
-      Priority       - Priority for MX type RRs
-      TTL_Sec        - 'ttl' value for the RR
-        * A value of zero indicates the domain default
-      Weight         - Weight for SRV type RRs
-      Port           - Port for SRV type RRs
-      
-    Returned fields:
-      RESOURCEID     - Unique identifier for the new or updated RR
+  @__api_request(required=['LinodeID'], optional=['pendingOnly'])
+  def linode_job_list(self, request):
+    pass
 
-    *** Parameters not passed to update an RR will be reset to defaults ***
-
-    """
-
-  @__api_request(['DomainID'])
-  def domainDelete(self, request):
-    """Delete a specific domain/zone
-
-    Parameters:
-      DomainID      - The unique identifier for this Domain/Zone
-
-    Returns DOMAINID
-
-    """
-
-  @__api_request(['ResourceID'])
-  def domainResourceDelete(self, request):
-    """Delete a specific resource record (RR).
-
-    Parameters:
-      ResourceID    - The unique identifier for this Resource Record
-
-    Returns RESOURCEID
-
-    """
+  @__api_request(optional=['isXen'])
+  def kernel_list(self, request):
+    pass
 
   @__api_request()
-  def linodeList(self, request):
-    """Retrieve the list of Linodes visible to the user.
-    
-    Parameters:
-      None
-      
-    Returns an array of hashes, one per Linode, each with the following fields:
-      LINODEID       - The unique identifier for this Linode
-      STATUS         - The Linode's status (see below)
-      HOSTHOSTNAME   - The DNS name for the host the Linode is on
-      LISHUSERNAME   - The username to connect to a Lish session
-      LABEL          - The label for the Linode, as seen on the Linode Manager
-      TOTALRAM       - Total RAM assigned to this Linode (MiB)
-      TOTALHD        - Total hard drive space assigned to this Linode (MiB)
-      TOTALXFER      - Total transfer assigned to this Linode (GiB)
-      
-    Possible values for STATUS
-      * This is still an undocumented API call
-      
-    """
+  def distribution_list(self, request):
+    pass
+
+  @__api_request()
+  def datacenter_list(self, request):
+    pass
