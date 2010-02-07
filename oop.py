5444dca6535a1fb8376127f39307fd1eb30ef45e
Timothy J Fontaine <tjfontaine@gmail.com>
2010-02-06 21:20:33 -0500

    refactor to support multiple api contexts, this needs more love

--- oop.py
+++ oop.py
@@ -5,9 +5,11 @@ from os import environ
 from api import Api, LowerCaseDict
 from fields import *
 
-_api = Api(environ['LINODE_API_KEY'])
+_api = Api('')
 
 _id_cache = {}
+
+apikey = ''
 
 class LinodeObject(object):
   fields = None
@@ -19,10 +21,11 @@ class LinodeObject(object):
   def __init__(self, entry={}):
     entry = dict([(str(k), v) for k,v in entry.items()])
     self.__entry = LowerCaseDict(entry)
+    self.__api = Api(apikey)
 
   def __getattr__(self, name):
     name = name.replace('_LinodeObject', '')
-    if name == '__entry':
+    if name in ('__entry', '__api'):
       return self.__dict__[name]
     elif not self.fields.has_key(name):
       raise AttributeError
@@ -35,7 +38,7 @@ class LinodeObject(object):
 
   def __setattr__(self, name, value):
     name = name.replace('_LinodeObject', '')
-    if name == '__entry':
+    if name in ('__entry', '__api'):
       object.__setattr__(self, name, value)
     elif not self.fields.has_key(name):
       raise AttributeError
@@ -58,10 +61,10 @@ class LinodeObject(object):
     if self.id:
       self.update()
     else:
-      self.id = self.create_method(**self.__entry)[self.primary_key]
+      self.id = self.create_method(self.__api, **self.__entry)[self.primary_key]
 
   def update(self):
-    self.update_method(**self.__entry)
+    self.update_method(self.__api, **self.__entry)
 
   @classmethod
   def __resolve_kwargs(self, kw):
@@ -78,7 +81,12 @@ class LinodeObject(object):
     if not _id_cache.has_key(self):
       _id_cache[self] = {}
 
-    for l in self.list_method(**kwargs):
+    try:
+      a = self.__api
+    except AttributeError:
+      self.__api = Api(apikey)
+
+    for l in self.list_method(self.__api, **kwargs):
       l = LowerCaseDict(l)
       o = self(l)
       o.cache_add()
@@ -104,8 +112,13 @@ class LinodeObject(object):
         result = v
         break
 
+    try:
+      a = self.__api
+    except AttributeError:
+      self.__api = Api(apikey)
+
     if not result:
-      result = LowerCaseDict(self.list_method(**kwargs)[0])
+      result = LowerCaseDict(self.list_method(self.__api, **kwargs)[0])
       o = self(result)
       o.cache_add()
       return o
@@ -129,7 +142,7 @@ class Datacenter(LinodeObject):
     'name'      : CharField('Location'),
   }
 
-  list_method = _api.avail_datacenters
+  list_method = Api.avail_datacenters
   primary_key =  'DatacenterID'
 
 class LinodePlan(LinodeObject):
@@ -141,7 +154,7 @@ class LinodePlan(LinodeObject):
     'xfer'    : IntField('Xfer'),
   }
 
-  list_method = _api.avail_linodeplans
+  list_method = Api.avail_linodeplans
   primary_key = 'PlanID'
 
 class Linode(LinodeObject):
@@ -172,25 +185,25 @@ class Linode(LinodeObject):
     'status'            : IntField('Status'),
   }
 
-  update_method = _api.linode_update
-  create_method = _api.linode_create
+  update_method = Api.linode_update
+  create_method = Api.linode_create
   primary_key   = 'LinodeID'
-  list_method   = _api.linode_list
+  list_method   = Api.linode_list
 
   def boot(self):
     ### TODO XXX FIXME return LinodeJob
-    return _api.linode_boot(linodeid=self.id)['JobID']
+    return self.__api.linode_boot(linodeid=self.id)['JobID']
 
   def shutdown(self):
     ### TODO XXX FIXME return LinodeJob
-    return _api.linode_shutdown(linodeid=self.id)['JobID']
+    return self.__api.linode_shutdown(linodeid=self.id)['JobID']
 
   def reboot(self):
     ### TODO XXX FIXME return LinodeJob
-    return _api.linode_reboot(linodeid=self.id)['JobID']
+    return self.__api.linode_reboot(linodeid=self.id)['JobID']
 
   def delete(self):
-    _api.linode_delete(linodeid=self.id)
+    self.__api.linode_delete(linodeid=self.id)
     self.cache_remove()
 
 class LinodeJob(LinodeObject):
@@ -208,7 +221,7 @@ class LinodeJob(LinodeObject):
     'pending_only'  : BoolField('PendingOnly'),
   }
 
-  list_method = _api.linode_job_list
+  list_method = Api.linode_job_list
   primary_key = 'JobID'
 
 class Distribution(LinodeObject):
@@ -221,7 +234,7 @@ class Distribution(LinodeObject):
     'created'   : DateTimeField('CREATE_DT'),
   }
 
-  list_method = _api.avail_distributions
+  list_method = Api.avail_distributions
   primary_key = 'DistributionID'
 
 class LinodeDisk(LinodeObject):
@@ -238,23 +251,23 @@ class LinodeDisk(LinodeObject):
     'readonly': BoolField('IsReadonly'),
   }
 
-  update_method = _api.linode_disk_update
-  create_method = _api.linode_disk_create
+  update_method = Api.linode_disk_update
+  create_method = Api.linode_disk_create
   primary_key   = 'DiskID'
-  list_method   = _api.linode_disk_list
+  list_method   = Api.linode_disk_list
 
   def duplicate(self):
-    ret = _api.linode_disk_duplicate(linodeid=self.linode.id, diskid=self.id)
+    ret = Api.linode_disk_duplicate(linodeid=self.linode.id, diskid=self.id)
     disk = LinodeDisk.get(linode=self.linode, id=ret['DiskID'])
     job = LinodeJob(linode=self.linode, id=ret['JobID'])
     return (disk, job)
 
   def resize(self, size):
-    ret = _api.linode_disk_resize(linodeid=self.linode.id, diskid=self.id, size=size)
+    ret = Api.linode_disk_resize(linodeid=self.linode.id, diskid=self.id, size=size)
     return LinodeJob.get(linode=self.linode, id=ret['JobID'])
 
   def delete(self):
-    ret = _api.linode_disk_delete(linodeid=self.linode.id, diskid=self.id)
+    ret = Api.linode_disk_delete(linodeid=self.linode.id, diskid=self.id)
     job = LinodeJob.get(linode=self.linode, id=ret['JobID'])
     self.cache_remove()
     return job
@@ -263,7 +276,7 @@ class LinodeDisk(LinodeObject):
   def create_from_distribution(self, linode, distribution, root_pass, label, size, ssh_key=None):
     l = ForeignField(Linode).to_linode(linode)
     d = ForeignField(Distribution).to_linode(distribution)
-    ret = _api.linode_disk_createfromdistribution(linodeid=l, distributionid=d,
+    ret = Api.linode_disk_createfromdistribution(linodeid=l, distributionid=d,
             rootpass=root_pass, label=label, size=size, rootsshkey=ssh_key)
     disk = self.get(id=ret['DiskID'], linode=linode)
     job = LinodeJob(id=ret['JobID'], linode=linode)
@@ -277,7 +290,7 @@ class Kernel(LinodeObject):
     'is_xen': BoolField('IsXen'),
   }
 
-  list_method = _api.avail_kernels
+  list_method = Api.avail_kernels
   primary_key = 'KernelID'
 
 class LinodeConfig(LinodeObject):
@@ -298,14 +311,14 @@ class LinodeConfig(LinodeObject):
     'helper_depmod'       : BoolField('helper_depmod'),
   }
 
-  update_method = _api.linode_config_update
-  create_method = _api.linode_config_create
+  update_method = Api.linode_config_update
+  create_method = Api.linode_config_create
   primary_key   = 'ConfigID'
-  list_method   = _api.linode_config_list
+  list_method   = Api.linode_config_list
 
   def delete(self):
     self.cache_remove()
-    _api.linode_config_delete(linodeid=self.linode.id, configid=self.id)
+    self.__api.linode_config_delete(linodeid=self.linode.id, configid=self.id)
 
 class LinodeIP(LinodeObject):
   fields = {
@@ -316,7 +329,7 @@ class LinodeIP(LinodeObject):
     'rdns'      : CharField('RDNS_NAME'),
   }
 
-  list_method = _api.linode_ip_list
+  list_method = Api.linode_ip_list
   primary_key = 'IPAddressID'
 
 class Domain(LinodeObject):
@@ -334,10 +347,10 @@ class Domain(LinodeObject):
     'master_ips': ListField('master_ips', type=CharField('master_ips')),
   }
 
-  update_method = _api.domain_update
-  create_method = _api.domain_create
+  update_method = Api.domain_update
+  create_method = Api.domain_create
   primary_key   = 'DomainID'
-  list_method   = _api.domain_list
+  list_method   = Api.domain_list
 
   STATUS_ON   = 1
   STATUS_EDIT = 2
@@ -345,7 +358,7 @@ class Domain(LinodeObject):
 
   def delete(self):
     self.cache_remove()
-    _api.domain_delete(domainid=self.id)
+    self.__api.domain_delete(domainid=self.id)
 
 class Resource(LinodeObject):
   fields = {
@@ -361,14 +374,14 @@ class Resource(LinodeObject):
     'ttl'       : IntField('TTL_sec'),
   }
 
-  update_method = _api.domain_resource_update
-  create_method = _api.domain_resource_create
+  update_method = Api.domain_resource_update
+  create_method = Api.domain_resource_create
   primary_key   = 'ResourceID'
-  list_method   = _api.domain_resource_list
+  list_method   = Api.domain_resource_list
 
   def delete(self):
     self.cache_remove()
-    _api.domain_resource_delete(domainid=self.domain.id, resourceid=self.id)
+    Api.domain_resource_delete(domainid=self.domain.id, resourceid=self.id)
 
 def _iter_class(self, results):
   _id_cache[self] = {}
