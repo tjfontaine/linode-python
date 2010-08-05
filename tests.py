a827279098ab2ef73778b15a76f738fedce9ed30
Josh Wright <jshwright@gmail.com>
2010-08-05 19:47:36 -0400

    Add a test case for test.echo

--- tests.py
+++ tests.py
@@ -12,6 +12,14 @@ class ApiTest(unittest.TestCase):
         available_plans = self.linode.avail_linodeplans()
         self.assertTrue(isinstance(available_plans, list))
 
+    def testEcho(self):
+        test_parameters = {'FOO': 'bar', 'FIZZ': 'buzz'}
+        response = self.linode.test_echo(**test_parameters)
+        self.assertTrue('FOO' in response)
+        self.assertTrue('FIZZ' in response)
+        self.assertEqual(test_parameters['FOO'], response['FOO'])
+        self.assertEqual(test_parameters['FIZZ'], response['FIZZ'])
+
 if __name__ == "__main__":
     if 'LINODE_API_KEY' not in os.environ:
         os.environ['LINODE_API_KEY'] = getpass('Enter API Key: ')
